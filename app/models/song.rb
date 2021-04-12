5698f9d20477dc5d80eb831e6b8639c0bde64d31
Ryan Erricson <ryanerricson@gmail.com>
2021-04-12 13:35:15 -0400

    commit tree dd81cc0fb1c882802b3685fbbeb7ab13514ed727

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def song_name_and_artist_name
+    artist.name + " - " + self.title
+  end
+
+  def artist_name
+    self.artist.name
+  end
 end
